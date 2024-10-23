098ee133508d2607b7188fd015f95409234f48af
Savannah Rogers <savannah.a.rogers@gmail.com>
2024-10-23 10:45:41 -0700

    Update nimbleJSrobustdesign.R

--- nimbleJSrobustdesign.R
+++ nimbleJSrobustdesign.R
@@ -24,8 +24,13 @@ dt <- diff(m$data()$time())
 
 
 #need functions sum, prod, which, expm::expm, usage,
-Rexpm <- nimbleRcall(function(x = double(1)){}, Rfun = 'add2',
-                     returnType = double(1))
+expmexpm <- function(x){
+  expm::expm(x)
+}
+Rexpm <- nimbleRcall(function(x = double(2)){}, Rfun = 'expmexpm',
+                     returnType = double(2))
+Rt <- nimbleRcall(function(x = double(2)){}, Rfun = 't',
+                  returnType = double(2))
 
 #spatial
 JS_SCR <- nimbleCode({
@@ -45,21 +50,14 @@ JS_SCR <- nimbleCode({
     } #tp
 
   # Dirichlet prior for entry probabilities
-  # for (t in 1:n.prim.occasions){
-  #   beta1[t] ~ dgamma(1, 1)
-  #   beta[t] <- beta1[t] / sum(beta1[1:n.occasions])
-  # } #t
+  ##Do I need a prior for alpha?
+  alpha[1:n.prim.occasions] <- rep(1, n.prim.occasions) 
   beta[1:n.prim.occasions] ~ ddirch(alpha[1:n.prim.occasions])
   
   # Convert entry probs to conditional entry probs
   b[1] <- beta[1]
   for (t in 2:n.prim.occasions){
-    prod1mb <- 1 #can't overwrite variables, will need to index and turn into vector
-    for(tt in 1:(t-1)){
-      prod1mb <- prod1mb * (1 - b[tt])
-    } #tt
-   b[t] <- beta[t] / prod1mb 
-  #b[t] <- beta[t]/prod(1 - b[1:(t-1)]) #is this the same?? rounding error?
+    b[t] <- beta[t]/prod(1 - b[1:(t-1)]) #is this vectorizing correctly?
   } #t
 
   # For prob available given alive, I need state transitions, including available and unavalaible states
@@ -72,22 +70,31 @@ JS_SCR <- nimbleCode({
   for (tp in 1:(n.prim.occasions - 1)){
     #get rid of array and t 
     Q[1:4,1:4,tp] <- c( #note recruitment index starts at 1, but transitions happen between occasions 
-    log(1 - b[(tp+1)])/dt[tp],   -log(1 - b[(tp+1)]*delta)/dt[tp],       -log(1 - b[(tp+1)]*(1 - delta))/dt[tp],  0,
-    0,                           -rho_au_Intercept[tp] + log(phi[tp]),   rho_au_Intercept[tp],                    -log(phi[tp]),
-    0,                           rho_ua_Intercept[tp],                   -rho_ua_Intercept[tp] + log(phi[tp]),    -log(phi[tp]),
-    0, 0, 0, 0
+      log(1 - b[(tp+1)])/dt[tp], 0,0,0,
+      -log(1 - b[(tp+1)]*delta)/dt[tp], -rho_au_Intercept[tp] + log(phi[tp]), rho_ua_Intercept[tp],          0,
+      -log(1 - b[(tp+1)]*(1 - delta))/dt[tp],  rho_au_Intercept[tp],      -rho_ua_Intercept[tp] + log(phi[tp]),  0,
+      0, -log(phi[tp]), -log(phi[tp]), 0
     )
+      #have to fill matrix by column, not row
+    #log(1 - b[(tp+1)])/dt[tp],   -log(1 - b[(tp+1)]*delta)/dt[tp],       -log(1 - b[(tp+1)]*(1 - delta))/dt[tp],  0,
+    #0,                           -rho_au_Intercept[tp] + log(phi[tp]),   rho_au_Intercept[tp],                    -log(phi[tp]),
+    #0,                           rho_ua_Intercept[tp],                   -rho_ua_Intercept[tp] + log(phi[tp]),    -log(phi[tp]),
+    #0, 0, 0, 0
+    #)
   G[1, 1:4, tp] <- c(1-b[(tp+1)], b[(tp+1)]*delta, b[(tp+1)]*(1-delta), 0) #from unborn
+  G[2:4, 1, tp] <- c(0,0,0) #cannot transition back to unborn
   #set expm as R function??
-  G[2:4,2:4,tp] <- as.array(expm::expm((Q[2:4,2:4,tp] * dt[tp]))) #matrix exponential for competing states
-  stateprob[(tp+1),1:4] <- stateprob[(tp),1:4] %*% G[1:4,1:4,tp] #membership probability 
+  Qd[1:3,1:3,tp] <- (Q[2:4,2:4,tp] * dt[tp])
+
+  G[2:4,2:4,tp] <- Rexpm(Qd[1:3,1:3,tp]) #matrix exponential for competing states
+#  stateprob[(tp+1),1:4] <- stateprob[(tp),1:4] %*% G[1:4,1:4,tp] #membership probability 
   }
   # Psi
-  for(tp in 1:(n.prim.occasions - 1)){
-    psi[tp] <- stateprob[tp,2]/sum(stateprob[tp,2:3]) #probability of being available given alive
-    alive[tp] <- stateprob[tp,2] + stateprob[tp,3]
-  }
-  
+#  for(tp in 1:(n.prim.occasions - 1)){
+#    psi[tp] <- stateprob[tp,2]/sum(stateprob[tp,2:3]) #probability of being available given alive
+#    alive[tp] <- stateprob[tp,2] + stateprob[tp,3]
+#  }
+}) 
   # Likelihood
   for (i in 1:M){
     Smesh[i] ~ dunif(1, nrow(mesh))
@@ -142,25 +149,31 @@ ch13 <- ch0[,primary %in% 1:3,]
 ch13 <- ch13[apply(ch13[,,-dim(ch13)[3]], 1, sum) > 0,,]
 usage.traps13 <- usage.traps[,primary %in% 1:3]
 primary13 <- primary[primary %in% 1:3]
+dt13 <-  dt[1:3]
 
 data <- list(y = ch13, 
              mesh = as.matrix(mesh),
              usage.traps = usage.traps13,
              distmat = distmat,
-             primary = primary13)
+             primary = primary13,
+             dt = dt13)
 
 constants <- list(n.prim.occasions = 3,
                   J = 91,
                   M = 700)
 
 inits <- list(
-  lambda0 = runif(1, 0, 1),
-  sigma = runif(1, 1, 1000),
-  phi = runif(n.prim.occasions), 
-  beta1 = rgamma(n.prim.occasions, 1),
-  rho_au_Intercept = runif(0, 20),
-  rho_ua_Intercept = runif(0, 20))
+  phi = m$get_par("phi", m = 1, j = 1, s = 1)[1:2], 
+  lambda0 = m$get_par("lambda0", m = 1, j = 1, s= 1, k = 1),
+  sigma = m$get_par("sigma", m = 1, j = 1, s= 1, k = 1),
+  delta = m$state()$delta()[1],
+  beta = m$get_par("beta", m = 1, j = 1, s = 1)[1:3], #rdirch(n.prim.occasions, 1), #rdirch for more than one n doesn't work
+  rho_ua_Intercept = apply(as.array(1:2), 1, function(x){m$state()$trm(k = (x+1))[2,1]}), #can't remember if its x or x+1
+  rho_au_Intercept = apply(as.array(1:2), 1, function(x){m$state()$trm(k = (x+1))[1,2]})
+)
 
 Rmodel <- nimbleModel(code = JS_SCR, 
                       constants = constants, 
-                      data = data)
+                      data = data,
+                      init = inits
+                      )
